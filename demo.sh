f497cbf6ccf7265c9f801d0b5f27a581693f0526
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2019-11-02 10:45:56 +0800

    update

---
+++ demo.sh
@@ -0,0 +1,1 @@
+k cp images helloworld-68d8c5897-5tnlk:/usr/share/nginx/html/
---
+++ patch-ingress.sh
@@ -0,0 +1,2 @@
+kubectl patch ing/hw-ingress --type=json \
+-p='[{"op": "replace", "path": "/spec/rules/0/host", "value":"hello.aks.cape.sh"}]'
